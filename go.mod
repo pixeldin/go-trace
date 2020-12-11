b842163e8e1bd501988ed97ef575d6a526a43de1
dinghan <dinghan@kingsoft.com>
2020-12-11 20:09:25 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-trace
+
+go 1.14
+
+require github.com/go-echarts/go-echarts/v2 v2.2.3
---
+++ trace/cmd.go
@@ -0,0 +1,8 @@
+package trace
+
+import "fmt"
+
+func TraceCmd()  {
+	// out put into console
+	fmt.Printf("PrintCmd")
+}
